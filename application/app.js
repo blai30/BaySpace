a91f354b8317d0907cbe41c2ed21c6912db13a6d
Brian Lai <brianlai30@outlook.com>
2019-06-28 16:30:56 -0700

    Moved all of the app files into subdirectory app/

---
+++ application/app.js
@@ -0,0 +1,72 @@
+// Import NodeJS core module
+const http = require('http');
+const express = require("express");
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/app/';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.use('/', router);
+
+router.get('/', function(req, res) {
+    res.sendFile(path + '/index.html');
+});
+
+router.get('/about', function(req, res) {
+    res.sendFile(path + '/about/index.html');
+});
+
+router.get('/signin', function(req, res) {
+    res.sendFile(path + '/signin/index.html');
+});
+
+router.get('/about/Anwar', function(req, res) {
+    res.sendFile(path + '/about/Anwar/index.html');
+});
+
+router.get('/about/Anya', function(req, res) {
+    res.sendFile(path + '/about/Anya/index.html');
+});
+
+router.get('/about/Brian', function(req, res) {
+    res.sendFile(path + '/about/Brian/index.html');
+});
+
+router.get('/about/Habtom', function(req, res) {
+    res.sendFile(path + '/about/Habtom/index.html');
+});
+
+router.get('/about/Jonathan', function(req, res) {
+    res.sendFile(path + '/about/Jonathan/index.html');
+});
+
+router.get('/about/Justin', function(req, res) {
+    res.sendFile(path + '/about/Justin/index.html');
+});
+
+router.get('/about/Sandeep', function(req, res) {
+    res.sendFile(path + '/about/Sandeep/index.html');
+});
+
+router.get('/about/Tianchen', function(req, res) {
+    res.sendFile(path + '/about/Tianchen/index.html');
+});
+
+app.listen(port, function() {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// // Create server and handle incoming requests
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// // Listen for incoming requests
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
