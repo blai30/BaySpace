03fbebe11494ba48cba56230e3376dc01de11a7f
Brian Lai <brianlai30@outlook.com>
2019-06-28 20:53:57 -0700

    Added routes and views

--- application/app.js
+++ application/app.js
@@ -1,17 +1,30 @@
 const express = require("express");
+const createError = require('http-errors');
+const path = require('path');
+
 const app = express();
 
-const path = __dirname + '/public';
-const port = 3000;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // Serve files statically
-app.use(express.static(path));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Display 404 page when attempting to view undefined page
-app.use("*",function(req,res) {
-    res.sendFile(path + "/404.html");
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, function() {
-    console.log(`Server running on Port: ${port}`);
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
