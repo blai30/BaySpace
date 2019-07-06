bd4911a95d96ef8fdf675a5be3bd1020dfe80c1f
Brian Lai <brianlai30@outlook.com>
2019-07-06 12:36:11 -0700

    Changed view engine from pug to handlebars

--- application/app.js
+++ application/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
 
 // Serve files statically
 app.use(express.static(path.join(__dirname, 'public')));
