90cf70a407c3891be617bd20d335bd2d4b552c5c
Brian Lai <brianlai30@outlook.com>
2019-07-25 21:14:21 -0700

    Integrated new layout with existing content

--- application/routes/index.js
+++ application/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Routes index.hbs page to / or /index (main page)
 router.get('/', (req, res, next) => {
   res.render('index', {
-    title: 'CSC 648 Team 05'
+    title: 'BaySpace | CSC 648 Team 05'
   });
 });
 
