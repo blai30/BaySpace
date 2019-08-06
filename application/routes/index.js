cebc5875ba1793a93c128f99873f948e85d5ed51
Brian Lai <brianlai30@outlook.com>
2019-08-05 22:28:55 -0700

    Added images for all of the locations. Converted index page into a single loop for all locations using sql query.

--- application/routes/index.js
+++ application/routes/index.js
@@ -4,13 +4,37 @@
  */
 
 const express = require('express');
+const database = require('../config/database');
 
 const router = express.Router();
 
+const title = 'BaySpace | CSC 648 Team 05';
+
 // Routes index.hbs page to / or /index (main page)
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'BaySpace | CSC 648 Team 05'
+  // Fetch all rows from the location table
+  let sqlQuery = 'SELECT * FROM location';
+
+  // Preview query in console
+  console.log(sqlQuery);
+
+  // Execute MySQL query in database to fetch results
+  database.query(sqlQuery, (err, result) => {
+    if (err) {
+      res.render({
+        title: title
+      });
+      next();
+    }
+
+    // Preview all fetched results
+    console.log(result);
+
+    // Render the page with all of the locations
+    res.render('index', {
+      title: title,
+      locations: result
+    });
   });
 });
 
